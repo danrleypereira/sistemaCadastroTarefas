c27827263714f01d9ce5c8c6162f04f19dce23a9
danrleywillyan <danrleywillian@gmail.com>
2018-11-05 08:01:18 -0200

    CRUD done

---
+++ pim8/Models/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pim8.Models
+{
+    public class Task
+    {
+        public int ID { get; set; }
+        public string fieldName { get; set; }
+        public DateTime dateTimeStart { get; set; }
+        public DateTime dateTimeEnd { get; set; }
+        public string description { get; set; }
+    }
+}
