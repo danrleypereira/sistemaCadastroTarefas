2059044a5a16b15cc3d9c6068601982d11ee4cb3
danrleywillyan <danrleywillian@gmail.com>
2018-11-05 08:14:38 -0200

    Adding correctly display Task atribbutes.

--- pim8/Models/Task.cs
+++ pim8/Models/Task.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,13 @@ namespace pim8.Models
     public class Task
     {
         public int ID { get; set; }
+        [Display(Name = "Nome")]
         public string fieldName { get; set; }
+        [Display(Name = "Começo da Tarefa")]
         public DateTime dateTimeStart { get; set; }
+        [Display(Name = "Fim da Tarefa")]
         public DateTime dateTimeEnd { get; set; }
+        [Display(Name = "Descrição da Tarefa")]
         public string description { get; set; }
     }
 }
